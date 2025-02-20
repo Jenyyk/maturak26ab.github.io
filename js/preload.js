342bbe650d68ccf2e8d20ca0d66cd1f2efa148c4
ZenithMeetsNadir <martin-vlcek@post.cz>
2025-02-20 10:12:22 +0100

    fix: preloader single link in head

--- js/preload.js
+++ js/preload.js
@@ -5,12 +5,15 @@
 document.querySelectorAll("a").forEach((link) => {
   // Add a listener for hovering
   link.addEventListener("mouseover", () => {
-    // Prefetch its target on hover
-    const preloadLink = document.createElement("link");
+    // Search for present prefetch link in the head to avoid duplicates
+    if (!document.head.querySelector(`link[href="${link.getAttribute("href")}"]`)) {
+      // Prefetch its target on hover
+      const preloadLink = document.createElement("link");
 
-    preloadLink.setAttribute("rel", "prefetch");
-    preloadLink.setAttribute("href", link.getAttribute("href"));
+      preloadLink.setAttribute("rel", "prefetch");
+      preloadLink.setAttribute("href", link.getAttribute("href"));
 
-    document.head.appendChild(preloadLink);
+      document.head.appendChild(preloadLink);
+    }
   })
 })

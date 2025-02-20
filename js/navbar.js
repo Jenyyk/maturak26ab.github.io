7874664a6fbc7e17279fbc3bf9294df8ef5b0cd1
Jenyyk <jendotron2@gmail.com>
2025-02-20 22:39:20 +0100

    refactor: True single-line implementation of navbar insertion

--- js/navbar.js
+++ js/navbar.js
@@ -3,14 +3,14 @@ import { initPreload } from './preload.js';
 
 /**
  * Inserts navbar into DOM and handles secondary functionalities associated with it.
- * 
+ *
  * Simple single line implementation featuring sponsors subpage including preload and camel:
  * ```js
  *  <script type="module">import('/js/navbar.js').then(nb => nb.insertNavbar('sponsors'));</script>
  * ```
- * @param {*} selectedPage 
+ * @param {*} selectedPage
  */
-export async function createNavbar(selectedPage) {
+async function createNavbar(selectedPage) {
     await insertNavbar(selectedPage);
     initPreload();
     initCamel();
@@ -18,12 +18,12 @@ export async function createNavbar(selectedPage) {
 
 /**
  * Inserts navbar into DOM.
- * 
+ *
  * !!! IMPORTANT !!!
- * 
+ *
  * Any references to the navbar (and its DOM elements) must take place AFTER the navbar has been loaded.
  * Make sure to await this function or append then() for any script that depends on the navbar.
- * 
+ *
  * @param {*} selectedPage repository name of the corresponding html file
  */
 async function insertNavbar(selectedPage) {
@@ -42,7 +42,11 @@ async function insertNavbar(selectedPage) {
                     navElem.appendChild(navTriangle);
                 }
             });
-            
+
             document.body.prepend(navBar);
         });
 }
+
+// This monstrosity gets the file name dynamically, so no need to type it in every file
+const file = location.pathname.split("/").pop().split(".")[0] || "index";
+await createNavbar(file);

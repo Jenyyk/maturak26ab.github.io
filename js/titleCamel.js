8fb0cd33bdc04d8d0d23c35df1319c21e4194934
Jenyyk <jendotron2@gmail.com>
2025-03-03 10:49:22 +0100

    dev: title camel easter egg 🐪

---
+++ js/titleCamel.js
@@ -0,0 +1,21 @@
+import keyTrap from "./mousetrap.js"
+
+keyTrap.bind("amalka", camelWalk);
+
+const camelEmoji = "🐪";
+const pageTitle = document.querySelector("title").innerHTML;
+let running = false;
+
+async function camelWalk() {
+  if (running) return;
+  running = true;
+  for (let i = pageTitle.length - 1; i >= 0; i--) {
+    let newTitle = pageTitle.slice(0, i) + camelEmoji + pageTitle.slice(i + 1);
+    document.querySelector("title").innerHTML = newTitle;
+    await sleep(200);
+  }
+  document.querySelector("title").innerHTML = pageTitle;
+  running = false;
+}
+
+const sleep = delay => new Promise((resolve) => setTimeout(resolve, delay))

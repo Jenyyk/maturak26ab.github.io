068cba9b39858b0b5cffc48350463d2db06be5c5
Jenyyk <jendotron2@gmail.com>
2025-03-18 08:28:27 +0100

    fix: fix presentation switching for slow networks

--- js/aboutPresentation.js
+++ js/aboutPresentation.js
@@ -1,4 +1,5 @@
-const imageElement = document.getElementById("presentationImage");
+let imageElement = document.getElementById("presentationImage");
+const imageParent = document.getElementById("presentation");
 const slideAmount = 5;
 let imageIndex = 0
 
@@ -12,6 +13,9 @@ function handleNext() {
   left.style.opacity = "1";
   if (imageIndex === slideAmount - 1) { return; }
   ++imageIndex;
+  imageElement.remove();
+  imageElement = document.createElement("img");
+  presentationDiv.prepend(imageElement);
   imageElement.setAttribute("src", `/assets/presentation/${imageIndex}.png`);
 }
 
@@ -20,5 +24,8 @@ function handlePrev() {
   right.style.opacity = "1";
   if (imageIndex === 0) { return; }
   --imageIndex;
+  imageElement.remove();
+  imageElement = document.createElement("img");
+  presentationDiv.prepend(imageElement);
   imageElement.setAttribute("src", `/assets/presentation/${imageIndex}.png`);
 }

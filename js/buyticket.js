60b0caae43bc5701a020a0dfa7a38fd235ec3a26
Dopple24 <roubradek1@gmail.com>
2025-10-22 19:47:04 +0200

    adds second checkbox to buyticket.html, buyticket.js and buyticket.css. Rework of buyticket.html. Creation of subpage podminky.html

--- js/buyticket.js
+++ js/buyticket.js
@@ -1,18 +1,18 @@
 const checkbox = document.getElementById("acceptCheckbox");
+const checkbox2 = document.getElementById("acceptCheckbox2");
 const amountSpan = document.getElementById("amountSpan");
 const emailInput = document.getElementById("emailInput");
 
 document.documentElement.style.setProperty("--blur-amount", (checkbox.checked) ? "0px" : "25px");
 checkbox.addEventListener("input", () => {
-  document.documentElement.style.setProperty("--blur-amount", (checkbox.checked && isEmailValid(emailInput.value)) ? "0px" : "25px");
+  document.documentElement.style.setProperty("--blur-amount", (checkbox.checked && checkbox2.checked && isEmailValid(emailInput.value)) ? "0px" : "25px");
 });
 emailInput.addEventListener("input", () => {
-  document.documentElement.style.setProperty("--blur-amount", (checkbox.checked && isEmailValid(emailInput.value)) ? "0px" : "25px");
+  document.documentElement.style.setProperty("--blur-amount", (checkbox.checked && checkbox2.checked && isEmailValid(emailInput.value)) ? "0px" : "25px");
 })
-document.getElementById("checkboxText").addEventListener("click", () => {
-  checkbox.checked = !checkbox.checked;
-  document.documentElement.style.setProperty("--blur-amount", (checkbox.checked && isEmailValid(emailInput.value)) ? "0px" : "25px");
-})
+checkbox2.addEventListener("input", () => {
+  document.documentElement.style.setProperty("--blur-amount", (checkbox.checked && checkbox2.checked && isEmailValid(emailInput.value)) ? "0px" : "25px");
+});
 
 import init, { get_qr } from "/js/qrpkg/wasm_qr.js";
 updateValues(1);
@@ -53,7 +53,7 @@ async function runWasm() {
   function updateQr(amount, email) {
     const bank_qr =
     "SPD*1.0*" +
-    "ACC:" + "CZ63" + "6210" + "670100" + "2222086531" +
+    "ACC:CZ2520100000002903183740" +
     `*AM:${amount}` +
     "*CC:CZK" +
     "*PT:IP" +
